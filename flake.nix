e040bb032264c056d58c30ef60cc82756273c3b7
Johan Sandred <johan.sandred@protonmail.com>
2024-03-31 12:32:41 +0200

    test

--- flake.nix
+++ flake.nix
@@ -13,8 +13,24 @@
       packages.${system} = {
         ${name} = pkgs.writeShellApplication {
           name = "${name}";
-          text = builtins.readFile ./nixrun.bash;
+          text = builtins.readFile ./src/nixrun.bash;
         };
+        # ${name} = pkgs.stdenv.mkDerivation {
+        #   src = ./;
+        #   name = "${name}";
+        #   inherit system;
+        #   dontUnpack = true;
+        #   installPhase = ''
+        #     cp $src $out
+        #     chmod +x $out
+        #   '';
+        #   postInstall = ''
+        #     # Avoid conflicts with other packages.
+        #     outfolder="$out/share/doc/${name}/"
+        #     mkdir -p "$outfolder"
+        #     mv $out/LICENSE "$outfolder"
+        #   '';
+        # };
         default = self.packages.${system}.${name};
       };
     };
